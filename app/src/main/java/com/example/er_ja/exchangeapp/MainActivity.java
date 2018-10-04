88ead4de06f49a944c14b2be44478c6529a20e37
pedrojoya <pedrojoya@gmail.com>
2018-10-04 09:37:51 +0200

    Sugerencias y correcciones

--- app/src/main/java/com/example/er_ja/exchangeapp/MainActivity.java
+++ app/src/main/java/com/example/er_ja/exchangeapp/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.er_ja.exchangeapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,44 +39,61 @@ public class MainActivity extends AppCompatActivity {
         rbFromDollar.setEnabled(false);
         rbFromDisabled = rbFromDollar;
 
+        // ME GUSTA ESTA IDEA.
         txtAmount.setOnClickListener(v -> txtAmount.selectAll());
+        // HAZ Code -> Reformat code PARA QUE EL CÓDIGO QUEDE MÁS BONITO.
         rgFrom.setOnCheckedChangeListener((group, checkedId) -> {
+            // DEFINE MEJOR UN MÉTODO Y LLÁMALO. EL CÓDIGO QUEDARÁ MUCHO MAS CLARO.
             if (checkedId==rbFromEuro.getId()){
                 rbToEuro.setEnabled(false);
                 rbToDisabled.setEnabled(true);
                 rbToDisabled=rbToEuro;
                 imgFrom.setImageResource(R.drawable.ic_euro);
+                // AÑADO ESTA LÍNEA PARA QUE PASE ALGÚN TEST. FÍJATE
+                imgFrom.setTag(R.drawable.ic_euro);
             }else if (checkedId==rbFromDollar.getId()){
                 rbToDollar.setEnabled(false);
                 rbToDisabled.setEnabled(true);
                 rbToDisabled=rbToDollar;
                 imgFrom.setImageResource(R.drawable.ic_dollar);
+                // AÑADO ESTA LÍNEA PARA QUE PASE ALGÚN TEST. FÍJATE
+                imgFrom.setTag(R.drawable.ic_dollar);
             }else{
                 rbToPound.setEnabled(false);
                 rbToDisabled.setEnabled(true);
                 rbToDisabled=rbToPound;
                 imgFrom.setImageResource(R.drawable.ic_pound);
+                // AÑADO ESTA LÍNEA PARA QUE PASE ALGÚN TEST. FÍJATE
+                imgFrom.setTag(R.drawable.ic_pound);
             }
 
         });
+        // EN LA ANTERIOR HAS USADO LAMBDA. POR QUÉ EN ESTA NO?
         rgTo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // DEFINE MEJOR UN MÉTODO Y LLÁMALO. EL CÓDIGO QUEDARÁ MUCHO MAS CLARO.
                 if (checkedId==rbToEuro.getId()){
                     rbFromEuro.setEnabled(false);
                     rbFromDisabled.setEnabled(true);
                     rbFromDisabled=rbFromEuro;
                     imgTo.setImageResource(R.drawable.ic_euro);
+                    // AÑADO ESTA LÍNEA PARA QUE PASE ALGÚN TEST. FÍJATE
+                    imgTo.setTag(R.drawable.ic_euro);
                 }else if (checkedId==rbToDollar.getId()){
                     rbFromDollar.setEnabled(false);
                     rbFromDisabled.setEnabled(true);
                     rbFromDisabled=rbFromDollar;
                     imgTo.setImageResource(R.drawable.ic_dollar);
+                    // AÑADO ESTA LÍNEA PARA QUE PASE ALGÚN TEST. FÍJATE
+                    imgTo.setTag(R.drawable.ic_dollar);
                 }else{
                     rbFromPound.setEnabled(false);
                     rbFromDisabled.setEnabled(true);
                     rbFromDisabled=rbFromPound;
                     imgTo.setImageResource(R.drawable.ic_pound);
+                    // AÑADO ESTA LÍNEA PARA QUE PASE ALGÚN TEST. FÍJATE
+                    imgTo.setTag(R.drawable.ic_pound);
                 }
             }
         });
@@ -95,12 +112,16 @@ public class MainActivity extends AppCompatActivity {
         rbToPound = findViewById(R.id.rbToPound);
         imgFrom = findViewById(R.id.imgFrom);
         imgTo = findViewById(R.id.imgTo);
+        // AÑADO ESTAS LÍNEAS PARA QUE PASE ALGUNOS TESTS. FÍJATE
+        imgFrom.setTag(R.drawable.ic_euro);
+        imgTo.setTag(R.drawable.ic_dollar);
     }
 
     private void exchange() {
         try{
             double num = Double.parseDouble(String.valueOf(txtAmount.getText()));
             double res;
+            // IN ENGLISH, PLEASE
             String monedaOrigen,monedaDestino;
             if(rbFromEuro.isChecked()){
                 monedaOrigen="€";
@@ -130,6 +151,7 @@ public class MainActivity extends AppCompatActivity {
                     res = num*GBP_USD;
                 }
             }
+            // EN VEZ DE USAR String.format(), USA RECURSO DE CADENA CON PARÁMETROS.
             Toast.makeText(this,String.format("%.2f %s = %.2f %s",num,monedaOrigen,res,monedaDestino),Toast.LENGTH_SHORT).show();
         }catch (Exception e){
             txtAmount.setText("0.00");
